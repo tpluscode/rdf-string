fb11c99f08f272d195a10358b59e23540a10356e
tpluscode <tpluscode@users.noreply.github.com>
2020-02-21 09:07:49 +0100

    feat(sparql): nested templates and null/undefined

---
+++ dist/node/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sparql_1 = require("./lib/sparql");
+exports.sparql = sparql_1.sparql;
---
+++ tests/jestSetup.ts
@@ -0,0 +1,47 @@
+import { Parser } from 'sparqljs'
+import { SparqlTemplateResult } from '../src/lib/sparql'
+
+const sparqlParser = new Parser()
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    interface Matchers<R> {
+      toMatchQuery(expected: string): R
+    }
+  }
+}
+
+expect.extend({
+  toMatchQuery(received: SparqlTemplateResult, expected: string) {
+    let expectedQuery: any
+    try {
+      expectedQuery = sparqlParser.parse(expected)
+    } catch (e) {
+      return {
+        pass: false,
+        message: () => `Failed to parse expected query:\n ${expected}`,
+      }
+    }
+    let actualQuery: any
+    try {
+      actualQuery = sparqlParser.parse(received.toString())
+    } catch (e) {
+      return {
+        pass: false,
+        message: () => `Failed to parse actual query. 
+ ${e.message}.
+ 
+ Query was:
+ ${received.toString()}`,
+      }
+    }
+
+    expect(actualQuery).toMatchObject(expectedQuery)
+
+    return {
+      pass: true,
+      message: () => 'Queries match',
+    }
+  },
+})
