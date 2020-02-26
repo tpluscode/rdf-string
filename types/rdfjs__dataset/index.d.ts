2e8795f35ff81e93f34d58a0e694f0cbfab44cbc
tpluscode <tpluscode@users.noreply.github.com>
2020-02-26 22:02:09 +0100

    chore: remove the temporary module declaration

--- types/rdfjs__dataset/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '@rdfjs/dataset' {
-  import { DatasetCore, DataFactory, DefaultGraph } from 'rdf-js'
-
-  const Dataset: {
-    dataset(): DatasetCore
-    defaultGraphInstance: DefaultGraph
-  } & Required<DataFactory>
-  export default Dataset
-}
