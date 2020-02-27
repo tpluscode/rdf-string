a63b0d94bf077c4b0b19af7b4628cce90cfed91e
tpluscode <tpluscode@users.noreply.github.com>
2020-02-27 17:02:32 +0100

    fix: allow any object to be interpolated value

--- src/lib/value.ts
+++ src/lib/value.ts
@@ -1,3 +1,3 @@
 import { DatasetCore, Quad, Term } from 'rdf-js'
 
-export type Value<TResult, TTerm extends Term = Term> = DatasetCore | Quad | TResult | TTerm | string | undefined | null | number | boolean | Date
+export type Value<TResult, TTerm extends Term = Term> = DatasetCore | Quad | TResult | TTerm | string | undefined | null | number | boolean | Date | object
