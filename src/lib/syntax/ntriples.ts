eb420cc3869861844a49b0518589feb9f884c48b
tpluscode <tpluscode@users.noreply.github.com>
2020-04-18 12:05:47 +0200

    fix: properly escape multiple literals and sepcial chars

--- src/lib/syntax/ntriples.ts
+++ src/lib/syntax/ntriples.ts
@@ -1,6 +1,28 @@
 import { BlankNode, Literal, NamedNode } from 'rdf-js'
 import xsd from './xsd'
 
+const echarRegEx = new RegExp('["\\\\\n\r]') // eslint-disable-line no-control-regex
+const echarRegExAll = new RegExp('["\\\\\n\r]', 'g') // eslint-disable-line no-control-regex
+
+const echarReplacement: Record<string, string> = {
+  '"': '\\"',
+  '\\': '\\\\',
+  '\n': '\\n',
+  '\r': '\\r',
+}
+
+function echarReplacer(char: string) {
+  return echarReplacement[char]
+}
+
+function escapeValue(value: string) {
+  if (echarRegEx.test(value)) {
+    return value.replace(echarRegExAll, echarReplacer)
+  }
+
+  return value
+}
+
 export function blankNode(term: BlankNode): string {
   return `_:${term.value}`
 }
@@ -9,8 +31,12 @@ export function namedNode(term: NamedNode): string {
   return `<${term.value}>`
 }
 
+export function literalValue(term: Literal) {
+  return `"${escapeValue(term.value)}"`
+}
+
 export function literal(term: Literal): string {
-  const value = `"${term.value}"`
+  const value = literalValue(term)
   if (term.language) {
     return `${value}@${term.language}`
   }
