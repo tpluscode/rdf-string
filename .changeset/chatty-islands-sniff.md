a93d42403b8facdfd8b6123b317b7a5ba4cd4591
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-09-23 11:10:28 +0200

    fix: serialising decimals

---
+++ .changeset/chatty-islands-sniff.md
@@ -0,0 +1,5 @@
+---
+"@tpluscode/rdf-string": patch
+---
+
+Improve serialization of literals where shorthand syntax is possible
--- src/lib/syntax/turtle.ts
+++ src/lib/syntax/turtle.ts
@@ -1,6 +1,7 @@
 import { BlankNode, Literal, NamedNode } from 'rdf-js'
 import { shrink } from '@zazuko/rdf-vocabularies/shrink'
 import type { NamespaceBuilder } from '@rdfjs/namespace'
+import TermMap from '@rdf-esm/term-map'
 import { PartialString } from '../TemplateResult'
 import { mapBuilders } from '../prefixes'
 import * as ntriples from './ntriples'
@@ -15,7 +16,7 @@ interface NamedNodeOptions {
   prefixes?: Record<string, string | NamespaceBuilder>
 }
 
-export function namedNode(term: NamedNode, { base = '', prefixes = {} }: NamedNodeOptions): PartialString {
+export function namedNode(term: Pick<NamedNode, 'value'>, { base = '', prefixes = {} }: NamedNodeOptions): PartialString {
   const baseStr = (typeof base === 'string') ? base : base.value
 
   const baseRegex = new RegExp('^' + baseStr)
@@ -35,31 +36,28 @@ export function namedNode(term: NamedNode, { base = '', prefixes = {} }: NamedNo
   }
 }
 
-function isBuiltInType(datatype: NamedNode): boolean {
-  return datatype.equals(xsd.integer) || datatype.equals(xsd.boolean) || datatype.equals(xsd.decimal)
-}
+const shorthandSyntaxPatterns = new TermMap<NamedNode, RegExp>([
+  [xsd.integer, /^-?[0-9]+$/],
+  [xsd.decimal, /^-?[0-9]+\.[0-9]+$/],
+  [xsd.boolean, /^(true|false)$/],
+])
 
 export function literal(term: Literal, { base = '', prefixes = {} }: NamedNodeOptions): PartialString {
-  if (!term.language && term.datatype) {
-    if (isBuiltInType(term.datatype)) {
-      return {
-        value: term.value,
-        prefixes: [],
-      }
-    }
+  const ntriplesString = ntriples.literal(term)
 
-    if (!term.datatype.equals(xsd.string)) {
-      const datatypeResult = namedNode(term.datatype, { base, prefixes })
+  // replace datatype with prefixed name
+  const [value, datatype] = ntriplesString.split('^^')
+  if (datatype && !shorthandSyntaxPatterns.get(term.datatype)?.test(term.value)) {
+    const datatypeResult = namedNode({ value: datatype.slice(1, -1) }, { base, prefixes })
 
-      return {
-        value: `${ntriples.literalValue(term)}^^${datatypeResult.value}`,
-        prefixes: datatypeResult.prefixes,
-      }
+    return {
+      value: `${value}^^${datatypeResult.value}`,
+      prefixes: datatypeResult.prefixes,
     }
   }
 
   return {
-    value: ntriples.literal(term),
+    value,
     prefixes: [],
   }
 }
